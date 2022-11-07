fb600fa8a23e4fffe969c5ccf0cc7faa32932fb3
Harrison <harrisonhough4@gmail.com>
2022-11-07 14:00:19 +0200

    feat: added RPM settings

---
+++ ReadyPlayerMeSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadyPlayerMeSettings : ScriptableObject
+{
+    public string partnerSubdomain;
+    public AvatarLoaderSettings AvatarLoaderSettings;
+}
