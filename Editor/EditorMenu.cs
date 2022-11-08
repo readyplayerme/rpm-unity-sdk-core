f948dd7d5295cabac7e94096dba933a3ca6cb7a1
Harrison <harrisonhough4@gmail.com>
2022-11-08 11:25:06 +0200

    chore: removed duplicate menu function

--- Editor/EditorMenu.cs
+++ Editor/EditorMenu.cs
@@ -4,12 +4,6 @@ namespace ReadyPlayerMe.Core.Editor
 {
     public static class EditorMenu
     {
-        [MenuItem("Ready Player Me/Avatar Loader", priority = 0)]
-        public static void OpenAvatarLoaderWindow()
-        {
-            //AvatarLoaderEditorWindow.ShowWindowMenu();
-        }
-
         [MenuItem("Ready Player Me/Settings", priority = 1)]
         public static void OpenSettingsWindow()
         {
