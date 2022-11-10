d219d4b87771a34260a4ccf9a15eb1fc6cf78057
Harrison <harrisonhough4@gmail.com>
2022-11-10 16:12:51 +0200

    chore: fixes and added request error check

--- Editor/PackageUpdater.cs
+++ Editor/PackageUpdater.cs
@@ -1,28 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEditor;
 using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.Networking;
 using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
-public static class PackageUpdater 
+public static class PackageUpdater
 {
     private const string PACKAGE_DOMAIN = "com.readyplayerme";
-    public class Release
-    {
-        [JsonProperty("tag_name")]
-        public string Tag;
-    }
 
     private const string GITHUB_WEBSITE = "https://github.com";
     private const string GITHUB_API_URL = "https://api.github.com/repos";
 
     public static void GetCurrentRelease()
     {
-        var packages = AssetDatabase.FindAssets("package") // Get all packages files
+        List<PackageInfo> packages = AssetDatabase.FindAssets("package") // Get all packages files
             .Select(AssetDatabase.GUIDToAssetPath) // Get path
             .Where(x => x.Contains("package.json") && x.Contains(PACKAGE_DOMAIN)) // Get package.json and com.ryuuk packages
             .Select(PackageInfo.FindForAssetPath).ToList();
@@ -33,7 +30,7 @@ public static class PackageUpdater
             return;
         }
 
-        var package = packages[0];
+        PackageInfo package = packages[0];
 
         // Get url of repository
         var repoUrl = package.packageId.Substring(package.name.Length + 1);
@@ -52,24 +49,31 @@ public static class PackageUpdater
 
     private static async void FetchReleases(string packageName, string packageUrl, string releasesUrl, Version currentVersion)
     {
-        var request = UnityWebRequest.Get(releasesUrl);
-        var async = request.SendWebRequest();
+        UnityWebRequest request = UnityWebRequest.Get(releasesUrl);
+        UnityWebRequestAsyncOperation async = request.SendWebRequest();
         while (!async.isDone)
         {
             await Task.Yield();
         }
+        if (request.isHttpError || request.isNetworkError)
+        {
+            Debug.Log($"Release Fetch Failed: Error {request.error} ");
+            Debug.Log($"Release Fetch Failed: Response {request.downloadHandler.text} ");
+            return;
+        }
 
         var response = request.downloadHandler.text;
 
-        var resp = JsonConvert.DeserializeObject<Release[]>(response);
-        var versions = new Version[resp?.Length ?? 0];
+        Release[] releases = JsonConvert.DeserializeObject<Release[]>(response);
 
-        for (int i = 0; i < resp.Length; i++)
+        var versions = new Version[releases?.Length ?? 0];
+
+        for (var i = 0; i < releases.Length; i++)
         {
-            versions[i] = new Version(resp[i].Tag.Substring(1));
+            versions[i] = new Version(releases[i].Tag);
         }
 
-        var latestVersion = versions.Max();
+        Version latestVersion = versions.Max();
 
         if (latestVersion > currentVersion)
         {
@@ -105,7 +109,7 @@ public static class PackageUpdater
 
     private static async void Update(string packageName, string packageUrl, Version currentVersion, Version latestVersion)
     {
-        var removeRequest = Client.Remove(packageName);
+        RemoveRequest removeRequest = Client.Remove(packageName);
         while (!removeRequest.IsCompleted)
         {
             await Task.Yield();
@@ -115,7 +119,7 @@ public static class PackageUpdater
 
         Debug.Log("[Updater] " + packageUrl);
 
-        var addRequest = Client.Add(packageUrl);
+        AddRequest addRequest = Client.Add(packageUrl);
         while (!addRequest.IsCompleted)
         {
             await Task.Yield();
@@ -123,5 +127,11 @@ public static class PackageUpdater
 
         Debug.Log($"Updated {packageName} from {currentVersion} to {latestVersion}");
         EditorPrefs.SetBool("inProgress", false);
+    }
+
+    public class Release
+    {
+        [JsonProperty("tag_name")]
+        public string Tag;
     }
 }
