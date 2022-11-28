fe3e9669ee0fe95e23a056928bb883ad207bde7b
Harrison <harrisonhough4@gmail.com>
2022-11-28 15:30:52 +0200

    test: new wait for install approach

--- Editor/ModuleInstaller.cs
+++ Editor/ModuleInstaller.cs
@@ -11,7 +11,7 @@ using UnityEditor.PackageManager;
 namespace ReadyPlayerMe.Core.Editor
 {
     [InitializeOnLoad]
-    public class ModuleInstaller : AssetPostprocessor
+    public class ModuleInstaller 
     {
         private const string PROGRESS_BAR_TITLE = "Ready Player Me";
 
@@ -19,6 +19,9 @@ namespace ReadyPlayerMe.Core.Editor
 
         static ModuleInstaller()
         {
+            var listRequest = Client.List(true);
+            while (!listRequest.IsCompleted)
+                Thread.Sleep(100);
             if (HasAnyMissingModule())
             {
                 EditorApplication.update += InstallModules;
@@ -49,7 +52,9 @@ namespace ReadyPlayerMe.Core.Editor
                 }
             }
             EditorAssetLoader.CreateSettingsAssets();
-            Thread.Sleep(200);
+            var listRequest = Client.List(true);
+            while (!listRequest.IsCompleted)
+                Thread.Sleep(100);
             EditorUtility.ClearProgressBar();
             ModuleInstallComplete?.Invoke();
             Debug.Log("ModuleInstaller END");
