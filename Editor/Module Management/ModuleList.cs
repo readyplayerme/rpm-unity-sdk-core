a824097f0153ab01185ea637d3ad91a5caecd672
Sercan Altundas <srcnalt@users.noreply.github.com>
2022-12-07 02:03:43 +0200

    chore: use feature branch for avatar loader module

--- Editor/Module Management/ModuleList.cs
+++ Editor/Module Management/ModuleList.cs
@@ -14,7 +14,7 @@
             {
                 name = "com.readyplayerme.avatarloader",
                 gitUrl = "https://github.com/readyplayerme/Unity-Avatar-Loader.git",
-                branch = "develop"
+                branch = "feature/cleanup" //"develop"
             },
             new ModuleInfo 
             {
