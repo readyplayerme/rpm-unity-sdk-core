b3cb1e5a7a73aa08549078ccc64de7e0fb6b2e15
Harrison <harrisonhough4@gmail.com>
2023-01-05 16:28:52 +0200

    chore: added missing summaries

--- Editor/EntryPoint.cs
+++ Editor/EntryPoint.cs
@@ -3,6 +3,9 @@ using UnityEditor;
 
 namespace ReadyPlayerMe.Core.Editor
 {
+    /// <summary>
+    ///     This class serves as a way of tracking when the Unity project (editor) is initially opened so that functions can be run at this point.
+    /// </summary>
     [InitializeOnLoad]
     public static class EntryPoint
     {
@@ -10,14 +13,17 @@ namespace ReadyPlayerMe.Core.Editor
 
         ///     Event for when package is imported or when project with package is opened.
         public static Action Startup;
-
+        
+        /// <summary>
+        ///     This constructor is used to subscribe to the <see cref="EditorApplication.update"/> event.
+        /// </summary>
         static EntryPoint()
         {
             EditorApplication.update += Update;
         }
 
         /// <summary>
-        ///     This function is called on every <c>EditorApplication.update</c>.
+        ///     This function is called on every Editor <see cref="EditorApplication.update"/>.
         ///     It is used to trigger moduleUpdater to check for updates when the Unity Project launches.
         /// </summary>
         private static void Update()
