5ded5b63517b7c51b95b87835291ac4d31adff18
Harrison <harrisonhough4@gmail.com>
2023-01-30 13:24:50 +1100

    feat: sdk logging using project prefs

--- Runtime/Logger/SDKLogger.cs
+++ Runtime/Logger/SDKLogger.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace ReadyPlayerMe.Core
 {
@@ -13,18 +14,40 @@ namespace ReadyPlayerMe.Core
     {
         public static readonly Logger AvatarLoaderLogger = new Logger(new CustomLogHandler());
 
+        private static readonly string LoggingEnabledPref = $"rpm-sdk-logging-enabled-{Application.dataPath.GetHashCode()}";
+  
+        private static bool loggingEnabled;
+
+        static SDKLogger()
+        {
+            loggingEnabled = GetEnabledPref();
+        }
+
+        public static bool GetEnabledPref()
+        {
+            return EditorPrefs.GetBool(LoggingEnabledPref, false);
+        }
+
+        public static void SetEnabledPref(bool enabled)
+        {
+            loggingEnabled = enabled;
+            EditorPrefs.SetBool(LoggingEnabledPref, loggingEnabled);
+        }
+        
         public static void Log(string tag, object message)
         {
-#if RPM_LOG
-            AvatarLoaderLogger.Log(tag, message);
-#endif
+            if (loggingEnabled)
+            {
+                AvatarLoaderLogger.Log(tag, message);
+            }
         }
 
         public static void LogWarning(string tag, object message)
         {
-#if RPM_LOG
-            AvatarLoaderLogger.LogWarning(tag, message);
-#endif
+            if (loggingEnabled)
+            {
+                AvatarLoaderLogger.LogWarning(tag, message);
+            }
         }
     }
 }
