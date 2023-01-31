f048b3e76f93a05f5afb74408569a027b628fbc0
Harrison Hough <harrisonhough4@gmail.com>
2023-01-31 06:26:51 +0200

    [3739587991] Feature/optional logging (#37)
    ## Changes
    
    #### Added
    
    -   Logging enabled setting for SDK logger

--- Runtime/Logger/SDKLogger.cs
+++ Runtime/Logger/SDKLogger.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace ReadyPlayerMe.Core
 {
@@ -13,14 +14,40 @@ namespace ReadyPlayerMe.Core
     {
         public static readonly Logger AvatarLoaderLogger = new Logger(new CustomLogHandler());
 
+        private static readonly string LoggingEnabledPref = $"rpm-sdk-logging-enabled-{Application.dataPath.GetHashCode()}";
+  
+        private static bool loggingEnabled;
+
+        static SDKLogger()
+        {
+            loggingEnabled = GetEnabledPref();
+        }
+
+        public static bool GetEnabledPref()
+        {
+            return EditorPrefs.GetBool(LoggingEnabledPref, false);
+        }
+
+        public static void SetEnabledPref(bool enabled)
+        {
+            loggingEnabled = enabled;
+            EditorPrefs.SetBool(LoggingEnabledPref, loggingEnabled);
+        }
+        
         public static void Log(string tag, object message)
         {
-            AvatarLoaderLogger.Log(tag, message);
+            if (loggingEnabled)
+            {
+                AvatarLoaderLogger.Log(tag, message);
+            }
         }
 
         public static void LogWarning(string tag, object message)
         {
-            AvatarLoaderLogger.LogWarning(tag, message);
+            if (loggingEnabled)
+            {
+                AvatarLoaderLogger.LogWarning(tag, message);
+            }
         }
     }
 }
