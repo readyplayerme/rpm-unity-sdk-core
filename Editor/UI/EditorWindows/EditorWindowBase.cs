03f24e7908a4789fb78828dc7054ec71b4e84eb7
Robin <1121080+rYuuk@users.noreply.github.com>
2023-05-08 17:24:01 +0200

    feat: remove redundant banner class

--- Editor/UI/EditorWindows/EditorWindowBase.cs
+++ Editor/UI/EditorWindows/EditorWindowBase.cs
@@ -15,18 +15,16 @@ namespace ReadyPlayerMe.Core.Editor
         private const string ERROR_ICON_SEARCH_FILTER = "t:Texture rpm_error_icon";
         private const float WIDTH = 460;
         private readonly GUILayoutOption windowWidth = GUILayout.Width(WIDTH);
-        private readonly Color descriptionColor = new Color(0.7f, 0.7f, 0.7f, 1.0f);
 
         protected readonly float ButtonHeight = 30f;
         
         protected GUIStyle HeadingStyle;
-        protected GUIStyle DescriptionStyle;
 
         protected Texture ErrorIcon;
 
         private GUIStyle webButtonStyle;
 
-        private Banner banner;
+        private Header header;
         private Footer footer;
 
         private string editorWindowName;
@@ -34,7 +32,7 @@ namespace ReadyPlayerMe.Core.Editor
 
         private void LoadAssets()
         {
-            banner ??= new Banner();
+            header ??= new Header();
 
             footer ??= new Footer(editorWindowName);
 
@@ -54,22 +52,10 @@ namespace ReadyPlayerMe.Core.Editor
                 fontSize = 14,
                 richText = true,
                 fontStyle = FontStyle.Bold,
-                margin = new RectOffset(5, 0, 0, 8),
+                margin = new RectOffset(15, 0, 0, 8),
                 normal =
                 {
                     textColor = Color.white
-                }
-            };
-
-            DescriptionStyle ??= new GUIStyle
-            {
-                fontSize = 12,
-                richText = true,
-                wordWrap = true,
-                margin = new RectOffset(5, 0, 0, 0),
-                normal =
-                {
-                    textColor = descriptionColor
                 }
             };
 
@@ -96,7 +82,7 @@ namespace ReadyPlayerMe.Core.Editor
                 GUILayout.FlexibleSpace();
                 Layout.Vertical(() =>
                 {
-                    banner.Draw(position);
+                    header.Draw(position);
                     content?.Invoke();
                     if (useFooter)
                     {
@@ -120,6 +106,7 @@ namespace ReadyPlayerMe.Core.Editor
             {
                 minSize = maxSize = new Vector2(WIDTH, height);
                 windowResized = true;
+                
             }
         }
     }
