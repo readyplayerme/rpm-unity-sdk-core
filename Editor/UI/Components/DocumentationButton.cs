21bb9e58182739315dfb578e444f4f8480eb5b0b
Robin <1121080+rYuuk@users.noreply.github.com>
2023-05-09 10:56:52 +0200

    feat: add documentation button class from avatar loader

---
+++ Editor/UI/Components/DocumentationButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ReadyPlayerMe.Core.Editor
+{
+    public static class DocumentationButton
+    {
+        public static void Draw(string url)
+        {
+            if (GUILayout.Button("?", new GUIStyle(GUI.skin.button)
+                {
+                    fontSize = 14,
+                    fixedHeight = 18,
+                    fixedWidth = 18,
+                    margin = new RectOffset(2, 0, 0, 8),
+                    normal =
+                    {
+                        textColor = new Color(0.7f, 0.7f, 0.7f, 1.0f)
+                    }
+                }))
+            {
+                Application.OpenURL(url);
+            }
+            EditorGUIUtility.AddCursorRect(GUILayoutUtility.GetLastRect(), MouseCursor.Link);
+        }
+    }
+}
