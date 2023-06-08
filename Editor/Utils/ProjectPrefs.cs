e666424b89cd7c1578553513c3fb7bef664bb9b1
Harrison Hough <harrisonhough4@gmail.com>
2023-06-08 13:14:20 +0300

    [SDK-185] Feature/setup guide fix (#79)
    ## Changes
    
    #### Updated
    
    -   added logic to module installer reset setup guide project pref

--- Editor/Utils/ProjectPrefs.cs
+++ Editor/Utils/ProjectPrefs.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public static class ProjectPrefs
 {
+    public const string FIRST_TIME_SETUP_DONE = "first-time-setup-guide";
+
     public static bool GetBool(string key)
     {
         return EditorPrefs.GetBool($"{Application.dataPath}{key}");
