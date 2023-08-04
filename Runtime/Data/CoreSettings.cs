daf3df2ecf77c0cfb662dd39d36bf4819fe2bbd6
Robin <1121080+rYuuk@users.noreply.github.com>
2023-08-04 13:23:33 +0200

    feat: add app id to core settings and settings window

--- Runtime/Data/CoreSettings.cs
+++ Runtime/Data/CoreSettings.cs
@@ -4,9 +4,11 @@ namespace ReadyPlayerMe.Core.Data
 {
     public class CoreSettings : ScriptableObject
     {
+        public const string DEFAULT_SUBDOMAIN = "demo";
+
         public string Subdomain = DEFAULT_SUBDOMAIN;
+        public string AppId = string.Empty;
         public bool EnableLogging;
         public bool EnableAnalytics;
-        public const string DEFAULT_SUBDOMAIN = "demo";
     }
 }
