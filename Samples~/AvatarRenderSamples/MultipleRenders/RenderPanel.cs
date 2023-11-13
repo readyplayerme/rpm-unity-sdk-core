f47abc5f64811b60c2b5b00cc6185773a2bf28c3
Robin <1121080+rYuuk@users.noreply.github.com>
2023-11-13 07:55:40 +0100

    [SDK-588] Update render api (#147)
    ## [SDK-588](https://ready-player-me.atlassian.net/browse/SDK-588)
    
    ## Description
    
    - Updated render api 
    - Updated samples and tests

--- Samples~/AvatarRenderSamples/MultipleRenders/RenderPanel.cs
+++ Samples~/AvatarRenderSamples/MultipleRenders/RenderPanel.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+using ReadyPlayerMe.Core;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,21 +6,22 @@ namespace ReadyPlayerMe.Samples
 {
     public class RenderPanel : MonoBehaviour
     {
-        public Text heading;
-        public Image image;
-
-        public void SetHeading(string text)
+        [SerializeField] private string url = "https://models.readyplayer.me/632d65e99b4c6a4352a9b8db.glb";
+        [SerializeField] private AvatarRenderSettings renderSettings;
+        [SerializeField] private Image image;
+        
+        private void Start()
         {
-            var headingText = string.Concat(text.Select(x => char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
-            heading.text = headingText;
+            var avatarRenderLoader = new AvatarRenderLoader();
+            avatarRenderLoader.OnCompleted = SetImage;
+            avatarRenderLoader.LoadRender(url, renderSettings);
         }
 
-        public void SetImage(Texture2D texture)
+        private void SetImage(Texture2D texture)
         {
             var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(.5f, .5f));
             image.sprite = sprite;
             image.preserveAspect = true;
         }
-
     }
 }
