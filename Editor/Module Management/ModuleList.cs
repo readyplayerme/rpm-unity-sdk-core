8bc4dfc7ed4add5479505f146d6aa5f08caa7230
Harrison <harrisonhough4@gmail.com>
2023-11-13 10:59:05 +0200

    chore: update version number

--- Editor/Module Management/ModuleList.cs
+++ Editor/Module Management/ModuleList.cs
@@ -14,7 +14,7 @@ namespace ReadyPlayerMe.Core.Editor
             name = "com.readyplayerme.core",
             gitUrl = "https://github.com/readyplayerme/rpm-unity-sdk-core.git",
             branch = "",
-            version = "4.0.0"
+            version = "4.0.1"
         };
 
         /// <summary>
