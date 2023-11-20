b512f15cfc47cef5139ee151964989a180853d84
Harrison Hough <harrisonhough4@gmail.com>
2023-11-20 11:43:26 +0200

    feat: change to move folder to prevent duplicate GUIDS

--- Samples~/WebGLSample/Editor/EditorWindows/SampleSetup.cs
+++ Samples~/WebGLSample/Editor/EditorWindows/SampleSetup.cs
@@ -18,7 +18,6 @@ namespace ReadyPlayerMe.Samples
         private const string CANCEL_BUTTON_TEXT = "Cancel";
 
         private const string RPM_WEBGL_SCREEN_SHOWN_KEY = "rpm-webgl-screen-shown";
-        private const string TEMPLATE_PATH = "WebGLTemplates";
         private const string FILE_NAME = "ReadyPlayerMe.Core.WebGLSample.asmdef";
         private const string PLUGINS_FOLDER = "Plugins";
         private const string WEBGL_HELPER_PATH = "WebGlHelper";
@@ -61,8 +60,8 @@ namespace ReadyPlayerMe.Samples
                 Debug.LogWarning("Failed to find WebGLSample. No changes were done to project");
                 return;
             }
-            CopyContents($"{samplesRootFolder}/{TEMPLATE_PATH}", $"{Application.dataPath}");
-            CopyContents($"{samplesRootFolder}/{WEBGL_HELPER_PATH}", $"{Application.dataPath}/{PLUGINS_FOLDER}");
+            MoveFolder($"{samplesRootFolder}/{TEMPLATE_PATH}", $"{Application.dataPath}");
+            MoveFolder($"{samplesRootFolder}/{WEBGL_HELPER_PATH}", $"{Application.dataPath}/{PLUGINS_FOLDER}");
             SetWebGLTemplate();
         }
 
@@ -77,7 +76,7 @@ namespace ReadyPlayerMe.Samples
             return rootSamplePath.TrimEnd('/');
         }
 
-        private static void CopyPath(string sourcePath, string destinationPath)
+        private static void MoveFolder(string sourcePath, string destinationPath)
     	{
         	// Extract the last part of the source path (e.g., "Plugin")
         	string sourceDirectoryName = new DirectoryInfo(sourcePath).Name;
@@ -91,29 +90,14 @@ namespace ReadyPlayerMe.Samples
             	throw new DirectoryNotFoundException("Source directory does not exist or could not be found: " + sourcePath);
         	}
 
-        	// Check if the new destination directory exists, if not, create it
-        	if (!Directory.Exists(newDestinationPath))
+        	// If the destination directory doesn't exist, create it
+        	if (!Directory.Exists(destinationPath))
         	{
-            	Directory.CreateDirectory(newDestinationPath);
+            	Directory.CreateDirectory(destinationPath);
         	}
 
-        	// Get the files in the directory and copy them to the new location
-        	string[] files = Directory.GetFiles(sourcePath);
-        	foreach (string file in files)
-        	{
-            	string fileName = Path.GetFileName(file);
-            	string destFile = Path.Combine(newDestinationPath, fileName);
-            	File.Copy(file, destFile, true); // true to overwrite if file already exists
-        	}
-
-        	// If copying subdirectories, copy them and their contents to new location
-        	string[] subdirectories = Directory.GetDirectories(sourcePath);
-        	foreach (string subdirectory in subdirectories)
-        	{
-            	string subdirectoryName = Path.GetFileName(subdirectory);
-            	string finalDestinationPath = Path.Combine(newDestinationPath, subdirectoryName);
-            	CopyPath(subdirectory, finalDestinationPath); // Recursively copy subdirectories
-        	}
+        	// Move the entire source directory to the new destination
+        	Directory.Move(sourcePath, newDestinationPath);
     	}
 
         private static void SetWebGLTemplate()
