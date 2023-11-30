2a1d7533aa082e1885d4c1b0525a37dc0c90f68a
MaxAndreassenRPM <max@wolf3d.io>
2023-11-30 10:21:06 +0000

    chore: add back catch clause with comment and description

--- Runtime/AvatarCreator/WebRequests/AuthorizedRequest.cs
+++ Runtime/AvatarCreator/WebRequests/AuthorizedRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using ReadyPlayerMe.Core;
 using UnityEngine.Networking;
@@ -21,8 +22,17 @@ namespace ReadyPlayerMe.AvatarCreator
 
             if (response is { IsSuccess: false, ResponseCode: 401 })
             {
-                await AuthManager.RefreshToken();
-
+                try
+                {
+                    await AuthManager.RefreshToken();
+                }
+                // Despite this seeming redundant, it prevents an issue with any error within AuthManager.RefreshToken not being propagated without it.
+                // TODO: Figure out why this is needed and work out a way to clean it up.
+                catch (Exception)
+                {
+                    throw;
+                }
+                
                 response = await Send<T>(requestData, ctx);
             }
 
