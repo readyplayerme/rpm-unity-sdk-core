2aa92399a59022f446d0eb261fbab3d3ffa1b506
Robin <1121080+rYuuk@users.noreply.github.com>
2023-12-01 14:21:43 +0530

    feat: add package imported event

--- Editor/Utils/PackageManager/PackageManagerEventListener.cs
+++ Editor/Utils/PackageManager/PackageManagerEventListener.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using ReadyPlayerMe.Core.Analytics;
 using UnityEditor;
 using UnityEditor.PackageManager;
@@ -8,16 +9,17 @@ namespace ReadyPlayerMe.Core.Editor
     public abstract class PackageManagerEventListener
     {
         private const string REQUIRED_KEYWORD = "readyplayerme";
-        
+        public static event Action<string> OnPackageImported;
+
         [InitializeOnLoadMethod]
         static void Initialize()
         {
-            Events.registeringPackages += OnPackagesInstalled;
+            Events.registeredPackages += OnPackagesInstalled;
         }
         
         ~PackageManagerEventListener()
         {
-            Events.registeringPackages -= OnPackagesInstalled;
+            Events.registeredPackages -= OnPackagesInstalled;
         }
 
         static void OnPackagesInstalled(PackageRegistrationEventArgs packageRegistrationEventArgs)
@@ -25,8 +27,11 @@ namespace ReadyPlayerMe.Core.Editor
             packageRegistrationEventArgs.added
                 .Where(packageInfo => packageInfo.packageId.Contains(REQUIRED_KEYWORD))
                 .ToList()
-                .ForEach(packageInfo => 
-                    AnalyticsEditorLogger.EventLogger.LogPackageInstalled(packageInfo.name, packageInfo.packageId));
+                .ForEach(packageInfo =>
+                {
+                    OnPackageImported?.Invoke(packageInfo.name);
+                    AnalyticsEditorLogger.EventLogger.LogPackageInstalled(packageInfo.name, packageInfo.packageId);
+                });
         }
     }
 }
