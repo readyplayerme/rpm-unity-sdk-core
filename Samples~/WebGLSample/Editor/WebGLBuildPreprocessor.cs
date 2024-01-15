109a2898125cd0bb31810df4a12adea61e734be5
Harrison Hough <harrisonhough4@gmail.com>
2024-01-15 11:10:28 +0200

    chore: fix logic

--- Samples~/WebGLSample/Editor/WebGLBuildPreprocessor.cs
+++ Samples~/WebGLSample/Editor/WebGLBuildPreprocessor.cs
@@ -15,7 +15,7 @@ namespace ReadyPlayerMe.Samples.WebGLSample.Editor
         private const string DONT_ASK_BUTTON_TEXT = "Don't ask again";
         private const string WARNING_TEXT =
             @"It looks like you are building for WebGL without the RpmWebGLTemplate. Would you like to import it now before building?";
-  
+
         private const string DONT_ASK_AGAIN_PREF = "RPM_DONT_ASK_AGAIN_WEBGL_TEMPLATE_WARNING";
         private const string INDEX_HTML_PATH = "Assets/WebGLTemplates/RPMTemplate/index.html";
         private const string RPM_WEB_HELPER = "RpmWebGLHelper.jslib";
@@ -25,28 +25,28 @@ namespace ReadyPlayerMe.Samples.WebGLSample.Editor
         public void OnPreprocessBuild(BuildReport report)
         {
 #if UNITY_WEBGL
-            if (ProjectPrefs.GetBool(DONT_ASK_AGAIN_PREF) && !Application.isBatchMode && File.Exists(INDEX_HTML_PATH) && IsWebHelperImported())
+            if (ProjectPrefs.GetBool(DONT_ASK_AGAIN_PREF) || Application.isBatchMode || !File.Exists(INDEX_HTML_PATH) || !IsWebHelperImported())
+                return;
+
+            var buttonOption = EditorUtility.DisplayDialogComplex(TITLE_TEXT,
+                WARNING_TEXT,
+                IMPORT_BUTTON_TEXT,
+                CONTINUE_BUTTON_TEXT,
+                DONT_ASK_BUTTON_TEXT);
+
+            switch (buttonOption)
             {
-
-                var buttonOption = EditorUtility.DisplayDialogComplex(TITLE_TEXT,
-                    WARNING_TEXT,
-                    IMPORT_BUTTON_TEXT,
-                    CONTINUE_BUTTON_TEXT,
-                    DONT_ASK_BUTTON_TEXT);
-
-                switch (buttonOption)
-                {
-                    case 0:
-                        WebGLPackageImporter.ImportPackage();
-                        WebGLPackageImporter.SetWebGLTemplate();
-                        break;
-                    case 2:
-                        ProjectPrefs.SetBool(DONT_ASK_AGAIN_PREF, true);
-                        break;
-                    default:
-                        break;
-                }
+                case 0:
+                    WebGLPackageImporter.ImportPackage();
+                    WebGLPackageImporter.SetWebGLTemplate();
+                    break;
+                case 2:
+                    ProjectPrefs.SetBool(DONT_ASK_AGAIN_PREF, true);
+                    break;
+                default:
+                    break;
             }
+
 #endif
         }
 
