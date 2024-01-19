2be9ef3f8ade534dc75f69061df9f5a699fd36cb
Raigo Kõvask <raigo.kovask@wolf3d.io>
2024-01-19 13:12:19 +0200

    feat: add webview tracking back

--- Editor/Core/Scripts/ModuleManagement/ModuleList.cs
+++ Editor/Core/Scripts/ModuleManagement/ModuleList.cs
@@ -18,6 +18,20 @@ namespace ReadyPlayerMe.Core.Editor
         };
 
         /// <summary>
+        ///     A static list of all the required modules represented in an array of <c>ModuleInfo</c>.
+        /// </summary>
+        public static readonly ModuleInfo[] Modules =
+        {
+            new ModuleInfo
+            {
+                name = "com.readyplayerme.webview",
+                gitUrl = "https://github.com/readyplayerme/rpm-unity-sdk-webview.git",
+                branch = "",
+                version = "2.1.3"
+            }
+        };
+
+        /// <summary>
         ///     Unity Module that adds support for gltf files that use DracoCompression.
         /// </summary>
         public static ModuleInfo DracoCompression = new ModuleInfo
@@ -38,6 +52,14 @@ namespace ReadyPlayerMe.Core.Editor
 
             var installedModules = new Dictionary<string, string>();
             installedModules.Add(Core.name, Core.version);
+
+            foreach (ModuleInfo module in Modules)
+            {
+                if (packageList.Any(x => x.name == module.name))
+                {
+                    installedModules.Add(module.name, module.version);
+                }
+            }
 
             if (packageList.Any(x => x.name == DracoCompression.name))
             {
