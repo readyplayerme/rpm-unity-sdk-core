e3996458c765926c29f902102628b386cae67491
Harrison Hough <harrisonhough4@gmail.com>
2024-02-08 10:46:58 +0200

    chore: remove develop from urls

--- Runtime/AvatarCreator/Scripts/Env/Env.cs
+++ Runtime/AvatarCreator/Scripts/Env/Env.cs
@@ -2,9 +2,9 @@
 {
     public class Env
     {
-        public const string RPM_SUBDOMAIN_BASE_URL = "https://{0}.develop.readyplayer.me/api";
-        public const string RPM_API_V2_BASE_URL = "https://develop.api.readyplayer.me/v2/";
-        public const string RPM_API_V1_BASE_URL = "https://develop.api.readyplayer.me/v1/";
-        public const string RPM_MODELS_BASE_URL = "https://develop.models.readyplayer.me";
+        public const string RPM_SUBDOMAIN_BASE_URL = "https://{0}.readyplayer.me/api";
+        public const string RPM_API_V2_BASE_URL = "https://api.readyplayer.me/v2/";
+        public const string RPM_API_V1_BASE_URL = "https://api.readyplayer.me/v1/";
+        public const string RPM_MODELS_BASE_URL = "https://models.readyplayer.me";
     }
 }
