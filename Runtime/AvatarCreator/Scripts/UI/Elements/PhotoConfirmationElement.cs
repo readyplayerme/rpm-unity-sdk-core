4651c4917e7ef1931f18fc66456d9e2ade419770
Harrison Hough <harrisonhough4@gmail.com>
2024-02-09 10:00:36 +0200

    [SDK-798] Feature/remove selfie element (#220)
    -   removed selfie to avatar element
    - minor PhotoCapture element UI fixes
    - Renamed Imageconfirmation component

--- Runtime/AvatarCreator/Scripts/UI/Elements/PhotoConfirmationElement.cs
+++ Runtime/AvatarCreator/Scripts/UI/Elements/PhotoConfirmationElement.cs
@@ -8,13 +8,14 @@ namespace ReadyPlayerMe.AvatarCreator
     /// A Unity MonoBehaviour class for displaying and confirming images.
     /// Allows users to set an image to be displayed and confirms it upon user interaction.
     /// </summary>
-    public class ImageConfirmationElement : MonoBehaviour
+    public class PhotoConfirmationElement : MonoBehaviour
     {
         [Header("Settings")]
-        [SerializeField] private RawImage imageTextureTarget;
+        [SerializeField] private RawImage photoTextureTarget;
 
+        [Space(5)]
         [Header("Events")]
-        public UnityEvent<Texture2D> OnImageConfirmed;
+        public UnityEvent<Texture2D> OnPhotoConfirmed;
 
         /// <summary>
         /// Sets the texture to be displayed in the element.
@@ -22,15 +23,15 @@ namespace ReadyPlayerMe.AvatarCreator
         /// <param name="texture">The Texture2D to be displayed.</param>
         public void SetTexture(Texture2D texture)
         {
-            imageTextureTarget.texture = texture;
+            photoTextureTarget.texture = texture;
         }
 
         /// <summary>
-        /// Confirms the currently displayed image and invokes the "onImageConfirmed" event with the confirmed texture.
+        /// Confirms the currently displayed image and invokes the "onPhotoConfirmed" event with the confirmed texture.
         /// </summary>
         public void ConfirmPhoto()
         {
-            OnImageConfirmed?.Invoke((Texture2D) imageTextureTarget.texture);
+            OnPhotoConfirmed?.Invoke((Texture2D) photoTextureTarget.texture);
         }
     }
 }
