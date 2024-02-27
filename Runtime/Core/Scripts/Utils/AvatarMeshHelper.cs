d5dfa0d8b29b5d48a7df30c33fe8e4802ee8e2a2
Sercan (Sarge) Altundas <srcnalt@users.noreply.github.com>
2024-02-27 14:35:06 +0200

    Mesh Helper multi-mesh support (#241)
    <!-- Copy the TICKETID for this task from Jira and add it to the PR name
    in brackets -->
    <!-- PR name should look like: [SDK-524] My Pull Request -->
    
    <!-- Add link for the ticket here editing the TICKETID-->
    
    ## [SDK-524](https://ready-player-me.atlassian.net/browse/SDK-524)
    
    ## Description
    
    - Mesh helper now supports transfer from multiple source mesh, material
    and bones target
    
    <!-- Fill the section below with Added, Updated and Removed information.
    -->
    <!-- If there is no item under one of the lists remove it's title. -->
    
    <!-- Testability -->
    
    ## How to Test
    
    - Load atlassed and non-atlassed avatars with different customization
    combinations in Photon Sample project
    
    <!-- Update your progress with the task here -->
    
    ## Checklist
    
    -   [ ] Tests written or updated for the changes.
    -   [ ] Documentation is updated.
    -   [x] Changelog is updated.
    
    <!--- Remember to copy the Changes Section into the commit message when
    you close the PR -->
    
    
    
    
    
    [SDK-524]:
    https://ready-player-me.atlassian.net/browse/SDK-524?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- Runtime/Core/Scripts/Utils/AvatarMeshHelper.cs
+++ Runtime/Core/Scripts/Utils/AvatarMeshHelper.cs
@@ -1,29 +1,68 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace ReadyPlayerMe.Core
 {
     public static class AvatarMeshHelper
     {
+        [Obsolete("Use TransferMesh(GameObject source, GameObject target) instead.")]
         public static void TransferMesh(GameObject source, SkinnedMeshRenderer[] targetMeshes, Animator targetAnimator)
         {
-            var sourceAnimator = source.GetComponentInChildren<Animator>();
-            SkinnedMeshRenderer[] sourceMeshes = source.GetComponentsInChildren<SkinnedMeshRenderer>();
-            for (var i = 0; i < targetMeshes.Length; i++)
+            TransferMesh(source, targetMeshes[0].transform.parent.gameObject);
+        }
+        
+        /// <summary>
+        ///     Transfers the mesh and material data from the source to the target avatar.
+        /// </summary>
+        /// <param name="source">Imported Ready Player Me avatar.</param>
+        /// <param name="target">Photon Network Player Character.</param>
+        public static void TransferMesh(GameObject source, GameObject target)
+        {
+            // store the relevant data of the source (downloaded) avatar
+            var rendererDict = new Dictionary<string, SkinnedMeshRenderer>();
+            
+            var sourceRenderers = source.GetComponentsInChildren<SkinnedMeshRenderer>();
+            var targetRenderers = target.GetComponentsInChildren<SkinnedMeshRenderer>();
+            
+            foreach (var renderer in sourceRenderers)
             {
-                if (i >= sourceMeshes.Length)
-                {
-                    targetMeshes[i].enabled = false;
-                    break;
-                }
-                Mesh mesh = sourceMeshes[i].sharedMesh;
-                targetMeshes[i].sharedMesh = mesh;
-                targetMeshes[i].enabled = true;
-                Material[] materials = sourceMeshes[i].sharedMaterials;
-                targetMeshes[i].sharedMaterials = materials;
+                rendererDict.Add(renderer.name, renderer);
             }
-
-            Avatar avatar = sourceAnimator.avatar;
-            targetAnimator.avatar = avatar;
+            
+            // transfer the data to the target skinning mesh renderers
+            foreach (var renderer in targetRenderers)
+            {
+                if (rendererDict.TryGetValue(renderer.name, out var sourceRenderer))
+                {
+                    renderer.material = sourceRenderer.materials[0];
+                    renderer.sharedMesh = sourceRenderer.sharedMesh;
+                    
+                    // transfer the bone data
+                    foreach (var targetBone in renderer.bones)
+                    {
+                        foreach (var sourceBone in sourceRenderer.bones)
+                        {
+                            if (sourceBone.name == targetBone.name)
+                            {
+                                targetBone.position = sourceBone.position;
+                                targetBone.rotation = sourceBone.rotation;
+                                targetBone.localScale = sourceBone.localScale;
+                                break;                            
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    // remove the renderer if it's not found in the source avatar
+                    Object.Destroy(renderer.gameObject);
+                }
+            }
+            
+            // transfer the animation avatar
+            var sourceAnimator = source.GetComponentInChildren<Animator>();
+            var targetAnimator = target.GetComponentInChildren<Animator>();
+            targetAnimator.avatar = sourceAnimator.avatar;
         }
     }
 }
