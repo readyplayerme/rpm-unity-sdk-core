a267a3b706c8df6e228e40d05bc091c27af49bf6
Sercan Altundas <srcnalt@users.noreply.github.com>
2024-02-27 14:37:01 +0200

    [feat] util for receiving head related mesh game objects

--- Runtime/Core/Scripts/Utils/AvatarMeshHelper.cs
+++ Runtime/Core/Scripts/Utils/AvatarMeshHelper.cs
@@ -1,10 +1,24 @@
+using System;
 using UnityEngine;
+using System.Linq;
 using System.Collections.Generic;
+using Object = UnityEngine.Object;
 
 namespace ReadyPlayerMe.Core
 {
     public static class AvatarMeshHelper
     {
+        private static readonly string[] headMeshNames = {
+            "Renderer_Head",
+            "Renderer_Hair",
+            "Renderer_Teeth",
+            "Renderer_Glasses",
+            "Renderer_EyeLeft",
+            "Renderer_EyeRight",
+            "Renderer_Headwear",
+            "Renderer_Facewear",
+        };
+
         [Obsolete("Use TransferMesh(GameObject source, GameObject target) instead.")]
         public static void TransferMesh(GameObject source, SkinnedMeshRenderer[] targetMeshes, Animator targetAnimator)
         {
@@ -63,6 +77,27 @@ namespace ReadyPlayerMe.Core
             var sourceAnimator = source.GetComponentInChildren<Animator>();
             var targetAnimator = target.GetComponentInChildren<Animator>();
             targetAnimator.avatar = sourceAnimator.avatar;
+        }
+        
+        /// <summary>
+        ///     Returns the meshes of the head of the avatar, such as glasses, hair, teeth, etc.
+        /// </summary>
+        /// <param name="avatar">A Ready Player Me Avatar.</param>
+        /// <returns>Head meshes in a GameObject Array.</returns>
+        public static GameObject[] GetHeadMeshes(GameObject avatar)
+        {
+            var renderers = avatar.GetComponentsInChildren<SkinnedMeshRenderer>();
+            var headMeshes = new List<GameObject>();
+            
+            foreach (var renderer in renderers)
+            {
+                if (headMeshNames.Contains(renderer.name))
+                {
+                    headMeshes.Add(renderer.gameObject);
+                }
+            }
+            
+            return headMeshes.ToArray();
         }
     }
 }
