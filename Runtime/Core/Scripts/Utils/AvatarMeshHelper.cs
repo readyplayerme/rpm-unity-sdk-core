d56ed81875c70a3a29c42421b9febb2524c2bf93
Sercan (Sarge) Altundas <srcnalt@users.noreply.github.com>
2024-03-04 15:26:36 +0200

    Release/v6.1.0 (#247)
    <!-- Copy the TICKETID for this task from Jira and add it to the PR name
    in brackets -->
    <!-- PR name should look like: [TICKETID] My Pull Request -->
    
    <!-- Add link for the ticket here editing the TICKETID-->
    
    ## [TICKETID](https://ready-player-me.atlassian.net/browse/TICKETID)
    
    ## Description
    ### Updated
    - AvatarMeshHelper now supports multiple mesh and material transfer.
    [#241](https://github.com/readyplayerme/rpm-unity-sdk-core/pull/241)
    
    ### Added
    - GetHeadMeshes method to AvatarMeshHelper to get head related meshes
    from an avatar.
    [#242](https://github.com/readyplayerme/rpm-unity-sdk-core/pull/242)
    - Template avatar with all possible meshes is added to the Resources
    folder.
    
    ## Checklist
    
    -   [x] Tests written or updated for the changes.
    -   [ ] Documentation is updated.
    -   [x] Changelog is updated.
    
    <!--- Remember to copy the Changes Section into the commit message when
    you close the PR -->
    
    ---------
    
    Co-authored-by: Harrison Hough <harrisonhough4@gmail.com>
    Co-authored-by: Raigo Kovask <raigo.kovask@gmail.com>
    Co-authored-by: Raigo Kõvask <107070960+rk132@users.noreply.github.com>

--- Runtime/Core/Scripts/Utils/AvatarMeshHelper.cs
+++ Runtime/Core/Scripts/Utils/AvatarMeshHelper.cs
@@ -1,29 +1,103 @@
+using System;
 using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ReadyPlayerMe.Core
 {
     public static class AvatarMeshHelper
     {
+        private static readonly string[] headMeshNames = {
+            "Renderer_Head",
+            "Renderer_Hair",
+            "Renderer_Hair",
+            "Renderer_Teeth",
+            "Renderer_Glasses",
+            "Renderer_EyeLeft",
+            "Renderer_EyeRight",
+            "Renderer_Headwear",
+            "Renderer_Facewear",
+        };
+
+        [Obsolete("Use TransferMesh(GameObject source, GameObject target) instead.")]
         public static void TransferMesh(GameObject source, SkinnedMeshRenderer[] targetMeshes, Animator targetAnimator)
         {
-            var sourceAnimator = source.GetComponentInChildren<Animator>();
-            SkinnedMeshRenderer[] sourceMeshes = source.GetComponentsInChildren<SkinnedMeshRenderer>();
-            for (var i = 0; i < targetMeshes.Length; i++)
+            TransferMesh(source, targetMeshes[0].transform.parent.gameObject);
+        }
+        
+        /// <summary>
+        ///     Transfers the mesh and material data from the source to the target avatar.
+        /// </summary>
+        /// <param name="source">Imported Ready Player Me avatar.</param>
+        /// <param name="target">Photon Network Player Character.</param>
+        public static void TransferMesh(GameObject source, GameObject target)
+        {
+            // store the relevant data of the source (downloaded) avatar
+            var rendererDict = new Dictionary<string, SkinnedMeshRenderer>();
+            
+            var sourceRenderers = source.GetComponentsInChildren<SkinnedMeshRenderer>();
+            var targetRenderers = target.GetComponentsInChildren<SkinnedMeshRenderer>();
+            
+            foreach (var renderer in sourceRenderers)
             {
-                if (i >= sourceMeshes.Length)
-                {
-                    targetMeshes[i].enabled = false;
-                    break;
-                }
-                Mesh mesh = sourceMeshes[i].sharedMesh;
-                targetMeshes[i].sharedMesh = mesh;
-                targetMeshes[i].enabled = true;
-                Material[] materials = sourceMeshes[i].sharedMaterials;
-                targetMeshes[i].sharedMaterials = materials;
+                rendererDict.Add(renderer.name, renderer);
             }
-
-            Avatar avatar = sourceAnimator.avatar;
-            targetAnimator.avatar = avatar;
+            
+            // transfer the data to the target skinning mesh renderers
+            foreach (var renderer in targetRenderers)
+            {
+                if (rendererDict.TryGetValue(renderer.name, out var sourceRenderer))
+                {
+                    renderer.sharedMesh = sourceRenderer.sharedMesh;
+                    renderer.sharedMaterial = sourceRenderer.sharedMaterial;
+                    
+                    // transfer the bone data
+                    foreach (var targetBone in renderer.bones)
+                    {
+                        foreach (var sourceBone in sourceRenderer.bones)
+                        {
+                            if (sourceBone.name == targetBone.name)
+                            {
+                                targetBone.position = sourceBone.position;
+                                targetBone.rotation = sourceBone.rotation;
+                                targetBone.localScale = sourceBone.localScale;
+                                break;                            
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    renderer.sharedMesh = null;
+                    renderer.material = null;
+                }
+            }
+            
+            // transfer the animation avatar
+            var sourceAnimator = source.GetComponentInChildren<Animator>();
+            var targetAnimator = target.GetComponentInChildren<Animator>();
+            targetAnimator.avatar = sourceAnimator.avatar;
+        }
+        
+        /// <summary>
+        ///     Returns the meshes of the head of the avatar, such as glasses, hair, teeth, etc.
+        /// </summary>
+        /// <param name="avatar">A Ready Player Me Avatar.</param>
+        /// <returns>Head meshes in a GameObject Array.</returns>
+        public static GameObject[] GetHeadMeshes(GameObject avatar)
+        {
+            var renderers = avatar.GetComponentsInChildren<SkinnedMeshRenderer>();
+            var headMeshes = new List<GameObject>();
+            
+            foreach (var renderer in renderers)
+            {
+                if (headMeshNames.Contains(renderer.name))
+                {
+                    headMeshes.Add(renderer.gameObject);
+                }
+            }
+            
+            return headMeshes.ToArray();
         }
     }
 }
