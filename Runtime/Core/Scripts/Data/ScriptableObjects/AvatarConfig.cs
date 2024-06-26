d73bd3e7afbb9ff3aaed520cd9aae21991e1002f
Harrison Hough <harrisonhough4@gmail.com>
2024-06-26 08:53:45 +0300

    Feature/remove resource assets (#303)
    Feature/remove resource assets (#303)

--- Runtime/Core/Scripts/Data/ScriptableObjects/AvatarConfig.cs
+++ Runtime/Core/Scripts/Data/ScriptableObjects/AvatarConfig.cs
@@ -15,7 +15,7 @@ namespace ReadyPlayerMe.Core
         [FormerlySerializedAs("MeshLod"), Tooltip("The mesh level of detail.")]
         public Lod Lod;
         [Tooltip("The resting pose of the avatars skeleton.")]
-        public Pose Pose;
+        public Pose Pose = Pose.TPose;
         [Tooltip("If set to NONE the mesh, materials and textures will not be combined into 1. (or 2 if an assets texture contains transparency)")]
         public TextureAtlas TextureAtlas;
         [Range(256, 1024), Tooltip("If set to none the mesh, materials and textures will not be combined into 1. (2 if an assets texture contains transparency)")]
