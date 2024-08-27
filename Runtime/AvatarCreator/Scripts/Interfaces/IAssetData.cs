2879f7ab72a56a4069672485df98a2efcea6e1d6
Eric Serrat <eric.serrat@gmail.com>
2024-08-27 13:25:25 +0200

    feat: preserve property Id from asset data (#313)
    [SRV-1333](https://ready-player-me.atlassian.net/browse/SRV-1333)
    
    ## Description
    
    - Preserve the Id from the IAsset interface, so when stripped in mobile
    it does not remove it
    
    ## How to Test
    
    - Build a sdk version for android using stripping level high, try to run
    it in the android emulator
    
    
    
    
    [SRV-1333]:
    https://ready-player-me.atlassian.net/browse/SRV-1333?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    Co-authored-by: Harrison Hough <harrisonhough4@gmail.com>
    Co-authored-by: Eric Serrat <eric@readyplayer.me>

--- Runtime/AvatarCreator/Scripts/Interfaces/IAssetData.cs
+++ Runtime/AvatarCreator/Scripts/Interfaces/IAssetData.cs
@@ -1,7 +1,10 @@
+using UnityEngine.Scripting;
+
 namespace ReadyPlayerMe.AvatarCreator
 {
     public interface IAssetData
     {
+        [Preserve]
         public string Id { get; set; }
         public AssetType AssetType { get; set; }
     }
