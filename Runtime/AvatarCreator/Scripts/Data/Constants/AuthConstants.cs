7babd6f7b8572f578c3116ad47519e1b31e7dcf1
Aivo Paas <aivo.paas@readyplayer.me>
2024-09-16 13:04:57 +0300

    [AUTH-6] Use auth-service endpoints for auth-related requests (#317)
    <!-- Copy the TICKETID for this task from Jira and add it to the PR name
    in brackets -->
    <!-- PR name should look like: [TICKETID] My Pull Request -->
    
    <!-- Add link for the ticket here editing the TICKETID-->
    
    ## [AUTH-6](https://ready-player-me.atlassian.net/browse/AUTH-6)
    
    ## Description
    
    - Switched from old authentication endpoints to the new ones in
    auth-service
    
    <!-- Fill the section below with Added, Updated and Removed information.
    -->
    <!-- If there is no item under one of the lists remove it's title. -->
    
    <!-- Testability -->
    
    ## How to Test
    
    -   Use the Avatar Creator Wizard functionality and verify it works:
        -   Log in with email code
        -   Sign up when clicking Next in the editor
    
    <!-- Update your progress with the task here -->
    
    ## Checklist
    
    -   [ ] Tests written or updated for the changes.
    -   [ ] Documentation is updated.
    -   [ ] Changelog is updated.
    
    <!--- Remember to copy the Changes Section into the commit message when
    you close the PR -->
    
    
    
    
    
    [AUTH-6]:
    https://ready-player-me.atlassian.net/browse/AUTH-6?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- Runtime/AvatarCreator/Scripts/Data/Constants/AuthConstants.cs
+++ Runtime/AvatarCreator/Scripts/Data/Constants/AuthConstants.cs
@@ -3,11 +3,10 @@
     public static class AuthConstants
     {
         public const string EMAIL = "email";
-        public const string AUTH_TYPE = "authType";
-        public const string AUTH_TYPE_CODE = "code";
-        public const string AUTH_TYPE_PASSWORD = "password";
+        public const string CODE = "code";
         public const string TOKEN = "token";
         public const string REFRESH_TOKEN = "refreshToken";
         public const string USER_ID = "id";
+        public const string APP_NAME = "appName";
     }
 }
---
+++ Runtime/AvatarCreator/Scripts/Data/CreatedUser.cs
@@ -0,0 +1,13 @@
+﻿namespace ReadyPlayerMe.AvatarCreator
+{
+    public struct CreatedUser
+    {
+        public string Id;
+        public string Name;
+        public string Email;
+        public string Token;
+        public string RefreshToken;
+        public string LastModifiedAvatarId;
+    }
+}
+
