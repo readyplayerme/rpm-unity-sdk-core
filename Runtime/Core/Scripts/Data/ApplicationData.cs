a4aaf1512523cff138c550f10ea463a91987ba4e
Harrison <harrisonhough4@gmail.com>
2024-10-30 08:00:18 +0200

    chore: update changelog

--- Runtime/Core/Scripts/Data/ApplicationData.cs
+++ Runtime/Core/Scripts/Data/ApplicationData.cs
@@ -6,7 +6,7 @@ namespace ReadyPlayerMe.Core
 {
     public static class ApplicationData
     {
-        public const string SDK_VERSION = "v7.3.0";
+        public const string SDK_VERSION = "v7.3.1";
         private const string TAG = "ApplicationData";
         private const string DEFAULT_RENDER_PIPELINE = "Built-In Render Pipeline";
         private static readonly AppData Data;
